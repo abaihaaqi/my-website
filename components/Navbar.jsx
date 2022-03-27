9f287182d6337fe6d2985a1ff54cda428275bcef
Ijay <saidhawwa1@gmail.com>
2022-03-27 09:59:25 +0700

    Navbar active link state 🛠️🎨

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
+import ActiveLink from "./ActiveLink";
 import { Factory, IdentificationCard, Newspaper, Palette } from "phosphor-react";
-import { navbarLink } from './Navbar.module.css'
+import { navbarLink, active } from './Navbar.module.css'
 
 export default function Navbar() {
   return (
-    <nav className='mx-auto flex justify-between items-center bg-accent lg:rounded-lg md:max-w-4xl absolute inset-x-0 z-30'>
+    <nav className='mx-auto pr-2 lg:pr-0 flex justify-between items-center bg-accent lg:rounded-lg md:max-w-4xl absolute inset-x-0 z-30'>
       <Link href="/">
         <a className={navbarLink}>
           <Image src="/icon/favicon-32x32.png" width={32} height={32} alt="Logo Nizar Baihaqi" />
@@ -15,33 +16,27 @@ export default function Navbar() {
         </a>
       </Link>
       <div className="flex">
-        {/* <Link href={'/design'}>
+        {/* <ActiveLink href={'/design'}>
           <a title="Design page" className={navbarLink}>{width > 500 ? 'Design' : <Palette size={32} />}</a>
-        </Link> */}
-        <Link href={'/project'}>
+        </ActiveLink> */}
+        <ActiveLink activeClassName={active} href={'/project'}>
           <a title="Project page" className={navbarLink}>
             <Factory size={32} />
-            <span className="hidden md:inline">
-              My Projects
-            </span>
           </a>
-        </Link>
-        <Link href={'/me'}>
+        </ActiveLink>
+        <ActiveLink activeClassName={active} href={'/me'}>
           <a title="About me" className={navbarLink}>
             <IdentificationCard size={32} />
-            <span className="hidden md:inline">
-              About Me
-            </span>
           </a>
-        </Link>
-        <Link href={'/case-study'}>
+        </ActiveLink>
+        <ActiveLink activeClassName={active} href={'/case-study'}>
           <a title="Case Study" className={navbarLink}>
             <Newspaper size={32} />
             <span className="hidden md:inline">
               Case Study
             </span>
           </a>
-        </Link>
+        </ActiveLink>
       </div>
     </nav>
   )
