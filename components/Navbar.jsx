64ddfdbe0d082f5b352e2a06d1eecbf3ad03f309
Ijay <saidhawwa1@gmail.com>
2022-03-27 08:09:11 +0700

    Fixed and improved design 🎨

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,51 +1,47 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { Factory, IdentificationCard, Palette } from "phosphor-react";
-import { useEffect, useState } from "react";
-import Button from "./Button";
+import { Factory, IdentificationCard, Newspaper, Palette } from "phosphor-react";
 import { navbarLink } from './Navbar.module.css'
 
 export default function Navbar() {
-  const router = useRouter()
-
-  const [width, setWidth] = useState(0)
-
-  function readWidth() {
-    setWidth(window.innerWidth)
-  }
-
-  useEffect(() => {
-    readWidth();
-  }, [])
-
   return (
-    <nav className='mx-auto flex justify-between items-center md:bg-accent lg:rounded-lg  md:max-w-4xl absolute inset-x-0 z-30'>
-      <div>
-        <Link href="/">
-          <a className={navbarLink}>
-            <Image src="/icon/favicon-32x32.png" width={32} height={32} alt="Logo Nizar Baihaqi" />
-            <span className="hidden md:inline">
-              Nizar Baihaqi
-            </span>
-          </a>
-        </Link>
-      </div>
+    <nav className='mx-auto flex justify-between items-center bg-accent lg:rounded-lg md:max-w-4xl absolute inset-x-0 z-30'>
+      <Link href="/">
+        <a className={navbarLink}>
+          <Image src="/icon/favicon-32x32.png" width={32} height={32} alt="Logo Nizar Baihaqi" />
+          <span className="hidden sm:inline">
+            Nizar Baihaqi
+          </span>
+        </a>
+      </Link>
       <div className="flex">
         {/* <Link href={'/design'}>
           <a title="Design page" className={navbarLink}>{width > 500 ? 'Design' : <Palette size={32} />}</a>
         </Link> */}
         <Link href={'/project'}>
-          <a title="Project page" className={navbarLink}>{width > 500 ? 'Project' : <Factory size={32} />}</a>
+          <a title="Project page" className={navbarLink}>
+            <Factory size={32} />
+            <span className="hidden md:inline">
+              My Projects
+            </span>
+          </a>
         </Link>
         <Link href={'/me'}>
-          <a title="About me" className={navbarLink}>{width > 500 ? 'Me' : <IdentificationCard size={32} />}</a>
+          <a title="About me" className={navbarLink}>
+            <IdentificationCard size={32} />
+            <span className="hidden md:inline">
+              About Me
+            </span>
+          </a>
         </Link>
-      </div>
-      <div className={navbarLink}>
-        <Button click={() => router.push('/case-study')}>
-          Case Study
-        </Button>
+        <Link href={'/case-study'}>
+          <a title="Case Study" className={navbarLink}>
+            <Newspaper size={32} />
+            <span className="hidden md:inline">
+              Case Study
+            </span>
+          </a>
+        </Link>
       </div>
     </nav>
   )
